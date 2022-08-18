dd24dae561aa45323fe7beb8397cd487575aefaa
Evans Ochieng <evansochieng121@gmail.com>
2022-08-18 13:45:27 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
 // Code your solution here
+
+// Get total number of batteries
+const totalBatteries = batteryBatches.reduce(
+    (battery, sum) => {
+        return sum + battery;
+    },
+    0
+);
